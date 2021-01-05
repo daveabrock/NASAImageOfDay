7180479a40b5b22dafb199883b1f1f714e1c0837
David Pine <david.pine@microsoft.com>
2021-01-05 14:27:10 -0600

    Update ImageGet.cs
    I did this from the GitHub.com UI, no IDE was used... I'm not even sure if this compiles, but this should be better. It uses `async & await`, avoid `.Result` and inlines the declaration and assingment.

--- BlastOff.Api/ImageGet.cs
+++ BlastOff.Api/ImageGet.cs
@@ -18,38 +18,34 @@ namespace BlastOff.Api
         public ImageGet(IRepository<Image> imageRepository) => _imageRepository = imageRepository;
 
         [FunctionName("ImageGet")]
-        public IActionResult Run(
+        public async ValueTask<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "image")] HttpRequest req,
             ILogger log)
         {
-
             bool hasDays = int.TryParse(req.Query["days"], out int days);
             log.LogInformation($"Requested images from last {days} days.");
 
             if (!hasDays && days <= 1 && days > 90)
                 return new BadRequestResult();
 
-            ValueTask<IEnumerable<Image>> imageResponse;
-            imageResponse = _imageRepository.GetAsync
+            var imageResponse = await _imageRepository.GetAsync
                  (img => img.Date > DateTime.Now.AddDays(-days));
 
-            return new OkObjectResult(imageResponse.Result);
+            return new OkObjectResult(imageResponse);
         }
 
         [FunctionName("ImageSearch")]
-        public IActionResult Search(
+        public async ValueTask<IActionResult> Search(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "search")] HttpRequest req,
             ILogger log)
         {
-
             var title = req.Query["title"];
             log.LogInformation($"Requested image containing title: {title}.");
 
-            ValueTask<IEnumerable<Image>> imageResponse;
-            imageResponse = _imageRepository.GetAsync
+            var imageResponse = await _imageRepository.GetAsync
                  (img => img.Title.Contains(title, StringComparison.InvariantCultureIgnoreCase));
 
-            return new OkObjectResult(imageResponse.Result);
+            return new OkObjectResult(imageResponse);
         }
     }
 }
