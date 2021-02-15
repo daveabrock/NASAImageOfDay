3a4f278dac49f48db78f7fad7bce7038c92bc622
Dave Brock <daveabrock@gmail.com>
2021-02-14 20:48:09 -0600

    fix build issue

--- BlastOff.Client/Program.cs
+++ BlastOff.Client/Program.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Threading.Tasks;
-using Blazored.Modal;
 using MudBlazor.Services;
 
 namespace BlastOff.Client
